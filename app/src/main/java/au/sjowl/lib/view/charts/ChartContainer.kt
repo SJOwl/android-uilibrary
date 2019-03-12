2070e22bfc73ab921b7993d70759e23a10de268f
vorobei <evgenii.vorobei@fulldive.com>
2019-03-12 16:42:02 +0200

    items

--- app/src/main/java/au/sjowl/lib/view/charts/ChartContainer.kt
+++ app/src/main/java/au/sjowl/lib/view/charts/ChartContainer.kt
@@ -3,6 +3,9 @@ package au.sjowl.lib.view.charts
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import androidx.annotation.ColorInt
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import org.jetbrains.anko.dip
 import org.jetbrains.anko.matchParent
 
@@ -22,6 +25,13 @@ class ChartContainer : LinearLayout {
 
     private var chart = TelegramChartView(context).apply {
         layoutParams = LayoutParams(matchParent, 0)
+    }
+
+//    private val chartsAdapter
+
+    private var recyclerView = RecyclerView(context).apply {
+        layoutManager = LinearLayoutManager(context)
+//        adapter = chartsAdapter
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -48,3 +58,37 @@ class ChartContainer : LinearLayout {
         init(context, attrs)
     }
 }
+
+data class ChartItem(
+    val name: String,
+    @ColorInt val color: Int
+)
+
+
+//interface ChartItemHolderListener {
+//    fun onClick(data: ChartItem)
+//}
+//
+//class ChartItemAdapter(
+//    private val onItemClickListener: ChartItemHolderListener
+//) : BaseRecyclerViewAdapter<ChartItem, BaseViewHolder>() {
+//
+//    override fun getViewHolderLayoutId(viewType: Int): Int = R.layout.rv_item_chart_item
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+//        return ChartItemViewHolder(inflate(parent, viewType), onItemClickListener)
+//    }
+//}
+//
+//class ChartItemViewHolder(
+//    view: View,
+//private val listener: ChartItemHolderListener
+//) : BaseViewHolder(view) {
+//
+//    override fun bind(item: Any) {
+//        (item as ChartItem)
+//        with(itemView){
+//            setOnClickListener { listener.onClick(item) }
+//        }
+//    }
+//}
